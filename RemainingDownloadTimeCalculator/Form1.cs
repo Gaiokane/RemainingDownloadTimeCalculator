30559c0555f71b1c295f5146339c2ec173980082
Gaiokane <qk632773809@vip.qq.com>
2020-02-28 22:43:43 +0800

    基本计算完成

--- RemainingDownloadTimeCalculator/Form1.cs
+++ RemainingDownloadTimeCalculator/Form1.cs
@@ -34,37 +34,58 @@ namespace RemainingDownloadTimeCalculator
         }
 
         /// <summary>
-        /// 根据文件大小和下载速度计算剩余下载时间，返回剩余时间（含单位）
+        /// 根据文件大小和下载速度计算剩余下载时间
         /// </summary>
         /// <param name="Size">文件大小，单位MB</param>
         /// <param name="Speed">下载速度，单位KB/s</param>
-        /// <returns></returns>
+        /// <returns>返回剩余时间（含单位）</returns>
         private string DownloadTime(double Size, double Speed)
         {
-            double time = Size * 1024 / Speed;//剩余多少秒
+            //MessageBox.Show("70/60:" + 59 / 60 + "\n70%60:" + 59 % 60);
+            double secondsRemaining = Size * 1024 / Speed;//剩余秒数
+            int minutesRemaining = Convert.ToInt32(secondsRemaining) / 60;//剩余分钟
+            int hoursRemaining = minutesRemaining / 60;//剩余小时
+            int daysRemaining = hoursRemaining / 24;//剩余天数
+
+
             //MessageBox.Show((time % 60).ToString());
-            if (time / 60 == 0)//不超过1分钟
+            if (secondsRemaining < 60)//不超过1分钟
             {
-                return time + "秒";
+                return secondsRemaining + "秒";
             }
             else//超过1分钟
             {
-                if (time / (60 * 60) == 0)//不超过1小时
+                if (minutesRemaining < 60)//不超过1小时
                 {
-                    return time / 60 + "分钟" + time % 60 + "秒";
+                    //double[] minsec = intdec(minutesRemaining);
+                    //MessageBox.Show("1:" + minsec[0] + "\n2:" + Math.Round(minsec[1]*60,7) + "\n3:" + Math.Ceiling(50.6));
+                    //return minsec[0] + "分钟" + Math.Ceiling(minsec[1] * 60) + "秒";
+                    return minutesRemaining + "分钟" + Math.Ceiling(secondsRemaining % 60) + "秒";
                 }
                 else//超过1小时
                 {
-                    if (time / (60 * 60 * 24) == 0)//不超过1天
+                    if (hoursRemaining < 24)//不超过1天
                     {
-                        return time / (60 * 60) + "小时" + time % (60 * 60) + "分钟" + time % 60 + "秒";
+                        return hoursRemaining + "小时" + minutesRemaining % 60 + "分钟" + Math.Ceiling(secondsRemaining % 60) + "秒";
                     }
                     else//超过1天
                     {
-                        return time / (60 * 60 * 24) + "天" + time % (60 * 60) + "小时" + time % (60 * 60) + "分钟" + time % 60 + "秒";
+                        return daysRemaining + "天" + hoursRemaining % 24 + "小时" + minutesRemaining % 60 + "分钟" + Math.Ceiling(secondsRemaining % 60) + "秒";
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// 传入小数，返回整数和小数部分
+        /// </summary>
+        /// <param name="num">需要分割的小数</param>
+        /// <returns>double数组，[0]为整数部分，[1]为小数部分</returns>
+        private double[] intdec(double num)
+        {
+            string[] temp = num.ToString().Split('.');
+            double[] result = { Convert.ToDouble(temp[0]), Convert.ToDouble(temp[1]) };
+            return result;
         }
     }
 }
